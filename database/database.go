de33aee8b8d1b87e780dd4706ee42f0739065700
Your Name <roihansori12@gmail.com>
2022-10-04 15:28:56 +0700

    fix(test) unit test API

--- database/database.go
+++ database/database.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,11 +14,12 @@ import (
 var DB *gorm.DB
 
 var (
-	DB_USERNAME string = util.GetConfig("DB_USERNAME")
-	DB_PASSWORD string = util.GetConfig("DB_PASSWORD")
-	DB_NAME     string = util.GetConfig("DB_NAME")
-	DB_HOST     string = util.GetConfig("DB_HOST")
-	DB_PORT     string = util.GetConfig("DB_PORT")
+	DB_USERNAME  string = util.GetConfig("DB_USERNAME")
+	DB_PASSWORD  string = util.GetConfig("DB_PASSWORD")
+	DB_NAME      string = util.GetConfig("DB_NAME")
+	DB_HOST      string = util.GetConfig("DB_HOST")
+	DB_PORT      string = util.GetConfig("DB_PORT")
+	DB_TEST_NAME string = util.GetConfig("DB_TEST_NAME")
 )
 
 func Connect() {
@@ -40,4 +42,84 @@ func Connect() {
 	log.Println("connected to the database")
 
 	DB.AutoMigrate(&model.User{}, &model.Book{})
+}
+
+type Config struct {
+	DB_Username  string
+	DB_Password  string
+	DB_Port      string
+	DB_Host      string
+	DB_Test_Name string
+}
+
+func InitTestDB() {
+	var err error
+
+	config := Config{
+		DB_Username:  "root",
+		DB_Password:  "root123",
+		DB_Port:      "3306",
+		DB_Host:      "localhost",
+		DB_Test_Name: "echo_book_test",
+	}
+
+	var dsn string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		config.DB_Username,
+		config.DB_Password,
+		config.DB_Host,
+		config.DB_Port,
+		config.DB_Test_Name,
+	)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatalf("error when connecting to the database: %s", err)
+	}
+
+	log.Println("connected to the database")
+	DB.AutoMigrate(&model.User{}, &model.Book{})
+}
+
+func SeedBook() model.Book {
+
+	var book model.Book = model.Book{
+		JudulBuku:   " Test",
+		Kategori:    "Testing",
+		Pengarang:   " test",
+		Penerbit:    " test",
+		TahunTerbit: " 2020",
+		Isbn:        121311414,
+	}
+
+	if err := DB.Create(&book).Error; err != nil {
+		panic(err)
+	}
+
+	var createdBook model.Book
+
+	DB.Last(&createdBook)
+
+	return createdBook
+}
+
+func SeedUser() model.User {
+	password, _ := bcrypt.GenerateFromPassword([]byte("123123"), bcrypt.DefaultCost)
+
+	var user model.User = model.User{
+		Email:    "testing@mail.com",
+		Password: string(password),
+	}
+
+	if err := DB.Create(&user).Error; err != nil {
+		panic(err)
+	}
+
+	var createdUser model.User
+
+	DB.Last(&createdUser)
+
+	createdUser.Password = "123123"
+
+	return createdUser
 }
--- go.mod
+++ go.mod
@@ -4,21 +4,22 @@ go 1.19
 
 require (
 	github.com/go-playground/validator/v10 v10.11.1
-	github.com/golang-jwt/jwt/v4 v4.4.2
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/jinzhu/gorm v1.9.16
 	github.com/labstack/echo/v4 v4.9.0
 	github.com/spf13/viper v1.13.0
+	github.com/stretchr/testify v1.8.0
 	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.10
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
@@ -30,6 +31,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
